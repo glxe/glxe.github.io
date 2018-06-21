412b90319d487db5f60eec4679713f0bcf1d7abc
glxe <gexiang@51ias.com>
2018-06-21 22:43:12 +0800

    [A

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ git status
 
 read -p "whether to continue? Any key to continue, press n key to exit! " go
 
-if [ $go == n ]; then
+if [ $go eq n ]; then
 exit 1;
 fi
 
@@ -12,6 +12,11 @@ git pull
 git add .
 
 read -p "Please enter git commit message: " c
+
+if [ " $c " ]; then
+
 git commit -m " $c "
 git push
 echo go on ...
+
+fi
